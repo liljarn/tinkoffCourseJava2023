abb492d7d6177d358e5a94c66f5cd2f93283f098
Maksim Ustinov <ustiinovmp@gmail.com>
2023-11-17 01:27:09 +0300

    version that could handle http on nginx and parse it

---
+++ src/main/java/edu/project3/formatter/Adoc.java
@@ -0,0 +1,10 @@
+package edu.project3.formatter;
+
+import edu.project3.model.metrics.Metric;
+
+public class Adoc implements Formatable{
+    @Override
+    public void print(Metric metric) {
+
+    }
+}
---
+++ src/main/java/edu/project3/model/metrics/MetricBuilder.java
@@ -0,0 +1,10 @@
+package edu.project3.model.metrics;
+
+import edu.project3.model.Log;
+import java.util.List;
+
+public interface MetricBuilder {
+    Metric build(
+        List<Log> logs
+    );
+}
