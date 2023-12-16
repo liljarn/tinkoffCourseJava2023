fe31c6fbb8f3242913b0fc7d36def702e867be92
Maksim Ustinov <ustiinovmp@gmail.com>
2023-12-17 02:09:36 +0300

    corrected git backslash issue

--- src/main/java/edu/hw10/task2/proxy/ProxyInvocationHandler.java
+++ src/main/java/edu/hw10/task2/proxy/ProxyInvocationHandler.java
@@ -16,8 +16,7 @@ import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class ProxyInvocationHandler implements InvocationHandler {
-    private final String[] pathStrings = {"src", "main", "resources", "hw10", "cached"};
-    private final Path pathToCachedMethods = Path.of(String.join("/", pathStrings));
+    private final Path pathToCachedMethods = Path.of("src", "main", "resources", "hw10", "cached");
     private final Map<Method, Map<List<Object>, Object>> map = new HashMap<>();
     private final Gson gson = new GsonBuilder().create();
 
@@ -35,7 +34,8 @@ public class ProxyInvocationHandler implements InvocationHandler {
                     Files.createDirectory(pathToCachedMethods);
                 }
 
-                Path fileName = pathToCachedMethods.resolve(method.getName() + "_" + createFileName(method));
+                Path fileName = pathToCachedMethods
+                    .resolve(method.getName() + "_" + createFileName(method) + ".txt");
                 if (!Files.exists(fileName)) {
                     Files.createFile(fileName);
                 }
--- src/test/java/edu/hw10/task2/CacheProxyTest.java
+++ src/test/java/edu/hw10/task2/CacheProxyTest.java
@@ -11,15 +11,12 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CacheProxyTest {
-
+    private final Path path = Path.of("src", "main", "resources", "hw10", "cached", "fib_num.txt");
+    private final Path dirPath = Path.of( "src", "main", "resources", "hw10", "cached");
 
     @SneakyThrows
     @BeforeEach
     void deleteDir() {
-        String[] pathStrings = {"src", "main", "resources", "hw10", "cached", "fib_num"};
-        Path path = Path.of(String.join( "/", pathStrings));
-        String[] pathDirStrings = {"src", "main", "resources", "hw10", "cached", "fib_num"};
-        Path dirPath = Path.of(String.join( "/", pathDirStrings));
         Files.deleteIfExists(path);
         Files.deleteIfExists(dirPath);
     }
@@ -28,8 +25,6 @@ public class CacheProxyTest {
     @Test
     @DisplayName("CacheProxy test with Cache (persist=true) annotation")
     public void proxyFib_shouldSaveResultOfFunctionFibOnDisk() {
-        String[] pathStrings = {"src", "main", "resources", "hw10", "cached", "fib_num"};
-        Path path = Path.of(String.join( "/", pathStrings));
         FibCalculator fibCalculator = new Fib();
         FibCalculator proxy = CacheProxy.create(fibCalculator, FibCalculator.class);
         proxy.fib(10);
