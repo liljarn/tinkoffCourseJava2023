ab56f62faa571e166e2d11bfdbcaced0d4df8f14
Maksim Ustinov <ustiinovmp@gmail.com>
2023-12-17 01:23:15 +0300

    added proper tests for task2

--- src/test/java/edu/hw10/task2/CacheProxyTest.java
+++ src/test/java/edu/hw10/task2/CacheProxyTest.java
@@ -1,16 +1,35 @@
 package edu.hw10.task2;
 
 import edu.hw10.task2.proxy.CacheProxy;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CacheProxyTest {
-    @Test
-    public void aboba() {
-        FibCalculator fibCalculator = new Fib();
-        FibCalculator proxy = CacheProxy.create(fibCalculator, FibCalculator.class);
-        assertThat(proxy.fib(10)).isEqualTo(proxy.fib(10));
+    private final Path path = Path.of("src/main/resources/hw10/cached/fib_num");
+    private final Path dirPath = Path.of("src/main/resources/hw10/cached");
+
+    @SneakyThrows
+    @BeforeEach
+    void deleteDir() {
+        Files.deleteIfExists(path);
+        Files.deleteIfExists(dirPath);
     }
 
-
+    @SneakyThrows
+    @Test
+    @DisplayName("CacheProxy test with Cache (persist=true) annotation")
+    public void proxyFib_shouldSaveResultOfFunctionFibOnDisk() {
+        FibCalculator fibCalculator = new Fib();
+        FibCalculator proxy = CacheProxy.create(fibCalculator, FibCalculator.class);
+        proxy.fib(10);
+        proxy.fib(9);
+        List<String> lines = Files.readAllLines(path);
+        assertThat(lines.get(2)).isEqualTo("[9]=34");
+    }
 }
