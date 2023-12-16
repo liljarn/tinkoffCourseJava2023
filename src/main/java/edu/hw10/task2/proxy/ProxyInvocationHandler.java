72286df39caeb78b52487eb9b436b9a89c2cbb68
Maksim Ustinov <ustiinovmp@gmail.com>
2023-12-17 01:38:07 +0300

    corrected git backslash issue

--- src/main/java/edu/hw10/task2/proxy/ProxyInvocationHandler.java
+++ src/main/java/edu/hw10/task2/proxy/ProxyInvocationHandler.java
@@ -16,7 +16,7 @@ import java.util.Properties;
 import java.util.stream.Collectors;
 
 public class ProxyInvocationHandler implements InvocationHandler {
-    private final Path pathToCachedMethods = Path.of("src/main/resources/hw10/cached");
+    private final Path pathToCachedMethods = Path.of("src", "main", "resources", "hw10", "cached");
     private final Map<Method, Map<List<Object>, Object>> map = new HashMap<>();
     private final Gson gson = new GsonBuilder().create();
 
